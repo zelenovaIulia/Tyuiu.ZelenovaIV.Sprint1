2822ab30eb373cd280bbe495a631b651f1759782
zelenovaIulia <zelenova_yulya@internet.ru>
2025-10-18 20:06:30 +0500

    rr

--- Tyuiu.ZelenovaIV.Sprint1.Task3.V10.Lib/DataService.cs
+++ Tyuiu.ZelenovaIV.Sprint1.Task3.V10.Lib/DataService.cs
@@ -7,8 +7,8 @@ namespace Tyuiu.ZelenovaIV.Sprint1.Task3.V10.Lib
         public string NumberToMoney(double number)
         {
             int rub = (int)number;
-            double kopeek = (int)((number - rub) * 100);
-            string sum = $"{rub}.{(number - rub) * 10}";
+            int kopeek = (int)Math.Round((number - rub) * 100);
+            string sum = $"{rub}.{(int)Math.Round((number - rub) * 100)}";
 
             return $"{sum} руб. - это {rub} руб. {kopeek} коп.";
         }
